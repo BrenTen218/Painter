401124cc25f481bb2b9fef696c05bd45df9bf8fe
BrenTen218 <brennans.deitz@gmail.com>
2021-04-23 19:30:30 -0400

    Initial commit

---
+++ src/PainterController.java
@@ -0,0 +1,136 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+public class PainterController {
+
+    private enum PenSize
+    {
+        SMALL(2),
+        MEDIUM(4),
+        MEDLARGE(5),
+        LARGE(6);
+
+        private final int radius;
+
+        PenSize(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public int getRadius()
+        {
+            return radius;
+        }
+    }
+
+    @FXML
+    private RadioButton blackRadioButton;
+
+    @FXML
+    private ToggleGroup colorToggleGroup;
+
+    @FXML
+    private RadioButton redRadioButton;
+
+    @FXML
+    private RadioButton greenRadioButton;
+
+    @FXML
+    private RadioButton blueRadioButton;
+
+    @FXML
+    private RadioButton yellowRadioButton;
+
+    @FXML
+    private RadioButton orangeRadioButton;
+
+    @FXML
+    private RadioButton smallRadioButton;
+
+    @FXML
+    private ToggleGroup sizeToggleGroup;
+
+    @FXML
+    private RadioButton mediumRadioButton;
+
+    @FXML
+    private RadioButton medlargeRadioButton;
+
+    @FXML
+    private RadioButton largeRadioButton;
+
+    @FXML
+    private Button undoButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Pane drawingAreaPane;
+
+
+    private PenSize radius = PenSize.MEDIUM;
+    private Paint brushColor = Color.BLACK;
+
+    public void initialize()
+    {
+        blackRadioButton.setUserData(Color.BLACK);
+        redRadioButton.setUserData(Color.RED);
+        greenRadioButton.setUserData(Color.GREEN);
+        blueRadioButton.setUserData(Color.BLUE);
+        yellowRadioButton.setUserData(Color.YELLOW);
+        orangeRadioButton.setUserData(Color.ORANGE);
+        smallRadioButton.setUserData(PenSize.SMALL);
+        mediumRadioButton.setUserData(PenSize.MEDIUM);
+        medlargeRadioButton.setUserData(PenSize.MEDLARGE);
+        largeRadioButton.setUserData(PenSize.LARGE);
+    }
+
+    @FXML
+    void clearButtonPressed(ActionEvent event)
+    {
+        drawingAreaPane.getChildren().clear();
+    }
+
+    @FXML
+    void colorRadioButtonSelected(ActionEvent event)
+    {
+        brushColor =
+                (Color) colorToggleGroup.getSelectedToggle().getUserData();
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event)
+    {
+        Circle newCircle = new Circle(event.getX(), event.getY(),
+                radius.getRadius(), brushColor);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
+
+    @FXML
+    void sizeRadioButtonSelected(ActionEvent event)
+    {
+        radius =
+                (PenSize) sizeToggleGroup.getSelectedToggle().getUserData();
+    }
+
+    @FXML
+    void undoButtonPressed(ActionEvent event)
+    {
+        int count = drawingAreaPane.getChildren().size();
+
+        if (count > 0)
+        {
+            drawingAreaPane.getChildren().remove(count - 1);
+        }
+    }
+
+}
